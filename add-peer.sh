4b27aac3aa987ec6db65b248007a8595b7d368ce
Naeim <31174546+snaeim@users.noreply.github.com>
2020-03-08 02:38:33 +0330

    support flag for add new client

--- add-peer.sh
+++ add-peer.sh
@@ -11,16 +11,43 @@ ALLOWEDIPS="0.0.0.0/0"
 PORT="51820"
 WGIF="wg0"
 
+while getopts n:s:i:e:p: option
+do
+	case $option in
+		n) NAME=${OPTARG};;		#config filename
+		s) SAVE=${OPTARG};;		#save flag
+		i) ADDRESS=${OPTARG};;		#internal ip address
+		e) ENDPOINT=${OPTARG};;		#server public ip address
+		p) SPUBLICKEY=${OPTARG};;	#server public key
+	esac
+done
+
+
 # Read internal address, server address and server public key from input
-read -p 'Internal Address = ' ADDRESS
-read -p 'Server Public IP = ' ENDPOINT
-read -p 'Server Public Key = ' SPUBLICKEY
+if [ "$ADDRESS" == '' ]
+then
+	read -p 'Internal Address = ' ADDRESS
+fi
+
+if [ "$ENDPOINT" == '' ]
+then
+	read -p 'Server Public IP = ' ENDPOINT
+fi
+
+if [ "$SPUBLICKEY" == '' ]
+then
+	read -p 'Server Public Key = ' SPUBLICKEY
+fi
+
 
 # Make peer config in to string
 CONFIG="[Interface]\nPrivateKey = ${PRIVATEKEY}\nAddress = ${ADDRESS}\nDNS = ${DNS}\n\n[Peer]\nPublicKey = ${SPUBLICKEY}\nAllowedIPs = ${ALLOWEDIPS}\nEndpoint = ${ENDPOINT}:${PORT}\n"
 
 # Get name of peer; use this name for config filename
-read -p 'For saving config file enter a name [Leave it blank for print in output]: ' NAME
+if [ "$NAME" == '' ]
+then
+	read -p 'For saving config file enter a name [Leave it blank for print in output]: ' NAME
+fi
 
 # Save config into file if have name for peer or print in output if dont have name
 if [ "$NAME" == '' ]
@@ -38,7 +65,10 @@ sudo wg set ${WGIF} peer ${PUBCLIEKEY} allowed-ips ${ADDRESS}
 printf "\nPeer with ${PUBCLIEKEY} public key added to ${WGIF}.\n"
 
 # Save this config permanently
-read -p 'Do you want save wireguard interface now? [y,n Default=n]: ' SAVE
+if [ "$SAVE" == '' ]
+then
+	read -p 'Do you want save wireguard interface now? [y,n Default=n]: ' SAVE
+fi
 if [ "$SAVE" == 'y' ]
 then
 	sudo wg-quick save ${WGIF}
