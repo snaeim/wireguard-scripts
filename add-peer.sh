d0ef9fdd09989932df92ecab8a6f5ce1967e36c9
Naeim <31174546+snaeim@users.noreply.github.com>
2021-05-11 16:39:04 +0430

    Update add-peer.sh

--- add-peer.sh
+++ add-peer.sh
@@ -3,73 +3,78 @@
 # Force to run as root
 [[ $UID == 0 ]] || { echo "You must be root to run this."; exit 1; }
 
-# Some default variable; you can change them if needed
-PRIVATEKEY=$(wg genkey)
-PUBCLIEKEY=$(wg pubkey <<< $PRIVATEKEY)
-DNS="8.8.8.8, 8.8.4.4"
-ALLOWEDIPS="0.0.0.0/0"
-PORT="51820"
-WGIF="wg0"
+# SERVER VAR
+SERVER_INTERFACE="wg0"
+SERVER_ENDPOINT="example.com"
+SERVER_PORT=$(wg show "${SERVER_INTERFACE}" | awk '/listening port:/' | sed -n 's/^.*: //p')
+SERVER_PUBLICKEY=$(wg show "${SERVER_INTERFACE}" | awk '/public key:/' | sed -n 's/^.*: //p')
 
-while getopts n:s:i:e:p: option
+# CLIENT VAR
+CLIENT_DNS="8.8.8.8, 8.8.4.4"
+CLEINT_ALLOWEDIPS="0.0.0.0/0"
+CLIENT_PRIVATEKEY=$(wg genkey)
+CLIENT_PUBCLIEKEY=$(wg pubkey <<< $CLIENT_PRIVATEKEY)
+
+
+while getopts i:n:h option
 do
 	case $option in
-		n) NAME=${OPTARG};;		#config filename
-		s) SAVE=${OPTARG};;		#save flag
-		i) ADDRESS=${OPTARG};;		#internal ip address
-		e) ENDPOINT=${OPTARG};;		#server public ip address
-		p) SPUBLICKEY=${OPTARG};;	#server public key
+		i) CLIENT_ADDRESS=${OPTARG};;		# get client ip address as arg
+		n) CLIENT_FILENAME=${OPTARG};;		# get client config filename
+		h) PRINT_HELP=1;;					# echo usage syntax
 	esac
 done
 
+# Find next IP address
+# after call this function ip address saved in NEXT_CLIENT_IP
+get_next_ip_address() {
+	LAST_CLIENT_IP=$(tail -1 /etc/wireguard/${SERVER_INTERFACE}.conf | grep -Pom 1 '[0-9.]{7,15}')
 
-# Read internal address, server address and server public key from input
-if [ "$ADDRESS" == '' ]
+	IFS='.'
+	read -ra LAST_CLIENT_IP <<< "${LAST_CLIENT_IP}"
+	LAST_CLIENT_IP[3]=$(expr ${LAST_CLIENT_IP[3]} + 1)
+	NEXT_CLIENT_IP=$(echo "${LAST_CLIENT_IP[*]}")
+}
+
+# Print inline syntax
+if [ "$PRINT_HELP" == 1 ]
 then
-	read -p 'Internal Address = ' ADDRESS
+	get_next_ip_address
+	BASH_SCRIPT_FILENAME=$(basename "$0") 
+	echo "sudo $BASH_SCRIPT_FILENAME -i CLIENT_IP_ADDRESS -n CLIENT_CONFIG_FILENAME"
+	echo "E.g. sudo $BASH_SCRIPT_FILENAME -i $NEXT_CLIENT_IP -n VPN-Client"
+	exit 1
 fi
 
-if [ "$ENDPOINT" == '' ]
+if [ "$CLIENT_ADDRESS" == '' ]
 then
-	read -p 'Server Public IP = ' ENDPOINT
+	get_next_ip_address
+	read -p "Internal Address [$NEXT_CLIENT_IP]: " CLIENT_ADDRESS
+	CLIENT_ADDRESS=${CLIENT_ADDRESS:-${NEXT_CLIENT_IP}}
 fi
+CLIENT_ADDRESS="${CLIENT_ADDRESS}/32"
 
-if [ "$SPUBLICKEY" == '' ]
-then
-	read -p 'Server Public Key = ' SPUBLICKEY
-fi
-
-
-# Make peer config in to string
-CONFIG="[Interface]\nPrivateKey = ${PRIVATEKEY}\nAddress = ${ADDRESS}\nDNS = ${DNS}\n\n[Peer]\nPublicKey = ${SPUBLICKEY}\nAllowedIPs = ${ALLOWEDIPS}\nEndpoint = ${ENDPOINT}:${PORT}\n"
-
-# Get name of peer; use this name for config filename
-if [ "$NAME" == '' ]
-then
-	read -p 'For saving config file enter a name [Leave it blank for print in output]: ' NAME
-fi
-
-# Save config into file if have name for peer or print in output if dont have name
-if [ "$NAME" == '' ]
-then
-	printf "\n\n\e[7m${CONFIG}\e[0m\n"
-else
-	NAME+=".conf"
-	printf "${CONFIG}" >> "${NAME}"
-	printf "\nYour config file saved to: $(pwd)/${NAME}"
-fi
-
+# Create Config File
+CONFIG="[Interface]\nPrivateKey = ${CLIENT_PRIVATEKEY}\nAddress = ${CLIENT_ADDRESS}\nDNS = ${CLIENT_DNS}\n\n[Peer]\nPublicKey = ${SERVER_PUBLICKEY}\nAllowedIPs = ${CLEINT_ALLOWEDIPS}\nEndpoint = ${SERVER_ENDPOINT}:${SERVER_PORT}\n"
+printf "\n\n\e[7m${CONFIG}\e[0m\n"
 
 # Add peer to wireguard interface
-sudo wg set ${WGIF} peer ${PUBCLIEKEY} allowed-ips ${ADDRESS}
-printf "\nPeer with ${PUBCLIEKEY} public key added to ${WGIF}.\n"
+wg set "${SERVER_INTERFACE}" peer "${CLIENT_PUBCLIEKEY}" allowed-ips "${CLIENT_ADDRESS}"
+printf "\nPeer with ${CLIENT_PUBCLIEKEY} public key added to ${SERVER_INTERFACE}.\n"
 
-# Save this config permanently
-if [ "$SAVE" == '' ]
+# Save new peer to interface file
+printf "\n[Peer]\nPublicKey = ${CLIENT_PUBCLIEKEY}\nAllowedIPs = ${CLIENT_ADDRESS}\n" >> /etc/wireguard/${SERVER_INTERFACE}.conf
+
+# Save Client config to a file
+if [ "$CLIENT_FILENAME" == '' ]
 then
-	read -p 'Do you want save wireguard interface now? [y,n Default=n]: ' SAVE
+	read -p 'For saving config file enter a name [or just leave it blank]: ' CLIENT_FILENAME
 fi
-if [ "$SAVE" == 'y' ]
+
+if [ "$CLIENT_FILENAME" != '' ]
 then
-	sudo wg-quick save ${WGIF}
+	CLIENT_FILENAME="$(pwd)/${CLIENT_FILENAME}.conf"
+	printf "${CONFIG}" > "${CLIENT_FILENAME}"
+	printf "\nYour config file saved to: ${CLIENT_FILENAME}\n"
+	chown $SUDO_USER:$SUDO_USER "${CLIENT_FILENAME}"
 fi
